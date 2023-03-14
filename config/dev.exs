d4e86a3b5976bdfa71d3e77fedebe7ab233c2993
johnhitz203 <johnhitz203@gmail.com>
2023-03-14 13:03:15 -0500

    Added Tailwind CSS

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :blog, BlogWeb.Endpoint,
   secret_key_base: "SvUMW6d76b7E5uB7G90FR81rXtC8FMIrjllzr7DBRB2FmH/k7ob5NiHSnPsLLQVC",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
--- mix.exs
+++ mix.exs
@@ -48,7 +48,12 @@ defmodule Blog.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:nimble_publisher, "~> 0.1.0"},
+      {:makeup_elixir, ">= 0.0.0"},
+      {:makeup_erlang, ">= 0.0.0"},
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+      {:tailwind_formatter, "~> 0.3.4", only: :dev, runtime: false}
     ]
   end
 
@@ -64,7 +69,8 @@ defmodule Blog.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"],
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
