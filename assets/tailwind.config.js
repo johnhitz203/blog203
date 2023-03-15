638c8ab3b343e432b93492a250f4e5044c31ae72
johnhitz203 <johnhitz203@gmail.com>
2023-03-14 20:43:58 -0500

    Styled the Phoenix landing page for practice

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -17,6 +17,13 @@ module.exports = {
     plugin(({addVariant}) => addVariant('phx-no-feedback', ['&.phx-no-feedback', '.phx-no-feedback &'])),
     plugin(({addVariant}) => addVariant('phx-click-loading', ['&.phx-click-loading', '.phx-click-loading &'])),
     plugin(({addVariant}) => addVariant('phx-submit-loading', ['&.phx-submit-loading', '.phx-submit-loading &'])),
-    plugin(({addVariant}) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &']))
+    // plugin(({addVariant}) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &'])),
+    // plugin(function( {addBase, theme}) {
+    //   addBase({
+    //     'h1': {fontSize: theme('fontSize.2xl')},
+    //     'h2': {fontSize: theme('fontSize.xl')},
+    //     'h3': {fontSize: theme('fontSize.lg')}
+    //   })
+    // })
   ]
 }
